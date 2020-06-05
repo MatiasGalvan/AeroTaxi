b2ef0fc56dcd4b9daef1ad3725f068443a9ff332
mauroemmi24 <emmi_81@live.com.ar>
2020-06-05 18:09:17 -0300

    Clase menu

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -4,10 +4,14 @@ import java.util.Scanner;
 
 public class Menu {
 
-    AeroTaxi sistema;
+    private AeroTaxi sistema;
 
     public Menu(AeroTaxi sistema) {
         this.sistema = sistema;
+    }
+
+    public Menu() {
+
     }
 
     public void inicio () {
@@ -21,6 +25,7 @@ public class Menu {
         System.out.println("Que quiere hacer?: "); res = scan.nextInt();
         switch (res) {
             case 1:
+                ingresar();
                 break;
             case 2:
                 registrarse();
@@ -31,10 +36,29 @@ public class Menu {
         }
     }
 
+    public void ingresar(){
+        Scanner scan = new Scanner(System.in);
+        int res=0;
+        String contraseña="";
+        Usuario usuario = new Usuario();
+        do {
+            System.out.println("Ingrese DNI:");
+            res = scan.nextInt();
+            usuario = sistema.validarUsuario(res);
+            if(usuario == null) System.out.println("El DNI ingresado no se encuentra en el sistema");
+        }while(usuario == null);
+
+        do {
+            System.out.println("Ingrese contraseña:");
+            contraseña = scan.nextLine();
+            if(contraseña!= usuario.getContraseña()) System.out.println("La contraseña es incorrecta");
+        }while(contraseña != usuario.getContraseña());
+        menuUsuario();
+    }
+
     public void registrarse () {
         Usuario usuario = new Usuario();
         Scanner scan = new Scanner(System.in);
-
         System.out.println("Ingrese nombre:");
         usuario.setNombre(scan.nextLine());
         System.out.println("Ingrese apellido:");
@@ -43,6 +67,8 @@ public class Menu {
         usuario.setDni(scan.nextInt());
         System.out.println("Ingrese edad:");
         usuario.setEdad(scan.nextInt());
+        System.out.println("Ingrese contraseña:");
+        usuario.setContraseña(scan.nextLine());
         sistema.agregarUsuario(usuario);
     }
 
@@ -50,4 +76,24 @@ public class Menu {
         System.exit(0);
     }
 
+    public void menuUsuario() {
+        Scanner scan = new Scanner(System.in);
+        int res = 0;
+        System.out.println("------------------------------");
+        System.out.println("1. Solicitar vuelo");
+        System.out.println("2. Cancelar vuelo");
+        System.out.println("3. Salir");
+        System.out.println("------------------------------");
+        System.out.println("Que quiere hacer?: "); res = scan.nextInt();
+        switch (res) {
+            case 1:
+
+                break;
+            case 2:
+                break;
+            case 3:
+                salir();
+                break;
+        }
+    }
 }
